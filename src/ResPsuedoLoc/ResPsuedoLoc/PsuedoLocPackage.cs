7906f187739f2246f9a3d096402488935b971bba
Matt Lacey <matt@mrlacey.co.uk>
2018-05-11 08:19:07 -0700

    Set basic context menu capability

---
+++ src/ResPsuedoLoc/ResPsuedoLoc/PsuedoLocPackage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
+
+
+// TODO: Add commands for all toggle options
+////> All XXXs
+////> ALL UPPERCASE
+////> L337
+////> AlTeRnAtE CaSe
+////> [! Surround !] (toggle)
+////> esreveR(toggle)
+////> iNVERT cASE(toggle)
+////> Add diacritics(toggle?)
+////> Add P-a-d-d-i-n-g(toggle)
+
+
+namespace ResPsuedoLoc
+{
+    [ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids.SolutionHasMultipleProjects)]
+    [ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids.SolutionHasSingleProject)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PsuedoLocPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class PsuedoLocPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "13097f34-2ebd-4ccc-bb05-bafad28a5c3b";
+
+        public static readonly Guid CommandSet = new Guid("ea9955ee-bcbf-4e98-a1a2-3ff72fe7746a");
+
+        public PsuedoLocPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await SurroundCommand.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
---
+++ src/ResPsuedoLoc/ResPsuedoLoc/SurroundCommand.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ResPsuedoLoc
+{
+    internal sealed class SurroundCommand
+    {
+        public const int CommandId = 4128;
+
+        private readonly AsyncPackage package;
+
+        private SurroundCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandId = new CommandID(PsuedoLocPackage.CommandSet, CommandId);
+            var menuItem = new OleMenuCommand(this.Execute, menuCommandId);
+            menuItem.BeforeQueryStatus += this.MenuItem_BeforeQueryStatus;
+            commandService.AddCommand(menuItem);
+        }
+
+        public static SurroundCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private string SelectedFileName { get; set; }
+
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Verify the current thread is the UI thread - the call to AddCommand in SurroundCommand's constructor requires
+            // the UI thread.
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new SurroundCommand(package, commandService);
+        }
+
+        private void MenuItem_BeforeQueryStatus(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sender is OleMenuCommand menuCmd)
+                {
+                    menuCmd.Visible = menuCmd.Enabled = false;
+
+                    if (!this.IsSingleProjectItemSelection(out var hierarchy, out var itemid))
+                    {
+                        this.SelectedFileName = null;
+                        return;
+                    }
+
+                    ((IVsProject)hierarchy).GetMkDocument(itemid, out var itemFullPath);
+                    var transformFileInfo = new FileInfo(itemFullPath);
+
+                    // Save the name of the selected file so we whave it when the command is executed
+                    this.SelectedFileName = transformFileInfo.FullName;
+
+                    if (transformFileInfo.Name.EndsWith(".resw")
+                     || transformFileInfo.Name.EndsWith(".resx"))
+                    {
+                        menuCmd.Visible = menuCmd.Enabled = true;
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                //this.Logger.RecordException(exc);
+                throw;
+            }
+        }
+
+
+        private bool IsSingleProjectItemSelection(out IVsHierarchy hierarchy, out uint itemid)
+        {
+            hierarchy = null;
+            itemid = VSConstants.VSITEMID_NIL;
+
+            var solution = Package.GetGlobalService(typeof(SVsSolution)) as IVsSolution;
+            if (!(Package.GetGlobalService(typeof(SVsShellMonitorSelection)) is IVsMonitorSelection monitorSelection) || solution == null)
+            {
+                return false;
+            }
+
+            var hierarchyPtr = IntPtr.Zero;
+            var selectionContainerPtr = IntPtr.Zero;
+
+            try
+            {
+                var hr = monitorSelection.GetCurrentSelection(out hierarchyPtr, out itemid, out var multiItemSelect, out selectionContainerPtr);
+
+                if (ErrorHandler.Failed(hr) || hierarchyPtr == IntPtr.Zero || itemid == VSConstants.VSITEMID_NIL)
+                {
+                    // there is no selection
+                    return false;
+                }
+
+                if (multiItemSelect != null)
+                {
+                    // multiple items are selected
+                    return false;
+                }
+
+                if (itemid == VSConstants.VSITEMID_ROOT)
+                {
+                    // there is a hierarchy root node selected, thus it is not a single item inside a project
+                    return false;
+                }
+
+                hierarchy = Marshal.GetObjectForIUnknown(hierarchyPtr) as IVsHierarchy;
+                if (hierarchy == null)
+                {
+                    return false;
+                }
+
+                if (ErrorHandler.Failed(solution.GetGuidOfProject(hierarchy, out var _)))
+                {
+                    // hierarchy is not a project inside the Solution if it does not have a ProjectID Guid
+                    return false;
+                }
+
+                // if we got this far then there is a single project item selected
+                return true;
+            }
+            finally
+            {
+                if (selectionContainerPtr != IntPtr.Zero)
+                {
+                    Marshal.Release(selectionContainerPtr);
+                }
+
+                if (hierarchyPtr != IntPtr.Zero)
+                {
+                    Marshal.Release(hierarchyPtr);
+                }
+            }
+        }
+
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "SurroundCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
