751c70c64e0832e9112afd27fa969789e74b9a01
Matt Lacey <matt@mrlacey.co.uk>
2018-11-09 19:21:24 +0000

    updated  some nuget packages to get references in sync with csproj

--- src/ResPsuedoLoc/ResPsuedoLoc/Commands/DoubleCommand.cs
+++ src/ResPsuedoLoc/ResPsuedoLoc/Commands/DoubleCommand.cs
@@ -34,9 +34,7 @@ namespace ResPsuedoLoc.Commands
 
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Verify the current thread is the UI thread - the call to AddCommand in DoubleCommand's constructor requires
-            // the UI thread.
-            ThreadHelper.ThrowIfNotOnUIThread();
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
             var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new DoubleCommand(package, commandService);
