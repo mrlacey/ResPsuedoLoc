205331b4b643e28fac706e21a14a721039e4a90a
Matt Lacey <matt@mrlacey.co.uk>
2018-12-27 17:47:57 +0000

    add uppercase tests

---
+++ src/ResPsuedoLoc/ResPsuedoLoc.Tests/UppercaseTests.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="UppercaseTests.cs" company="Matt Lacey Ltd.">
+// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// </copyright>
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResPsuedoLoc.Commands;
+
+namespace ResPsuedoLoc.Tests
+{
+    [TestClass]
+    public class UppercaseTests
+    {
+        [TestMethod]
+        public void SimpleFunctionalityWorksOk()
+        {
+            var source = "lowercase";
+
+            var actual = UppercaseCommand.UppercaseLogic(source);
+
+            Assert.AreEqual("LOWERCASE", actual);
+        }
+
+        [TestMethod]
+        public void DoesNothingIfAllAlreadyUppercase()
+        {
+            var source = "UPPERCASE";
+
+            var actual = UppercaseCommand.UppercaseLogic(source);
+
+            Assert.AreEqual("UPPERCASE", actual);
+        }
+
+        [TestMethod]
+        public void IgnoresSpacesAtEndsAndMiddle()
+        {
+            var source = " lower case ";
+
+            var actual = UppercaseCommand.UppercaseLogic(source);
+
+            Assert.AreEqual(" LOWER CASE ", actual);
+        }
+
+        [TestMethod]
+        public void HandlesEmptyString()
+        {
+            var actual = UppercaseCommand.UppercaseLogic(string.Empty);
+
+            Assert.AreEqual(string.Empty, actual);
+        }
+    }
+}
