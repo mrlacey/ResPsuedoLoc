735a83162ad65709725e1fbc26c13990b887cc67
Matt Lacey <matt@mrlacey.co.uk>
2023-11-21 00:35:27 +0000

    v3.5.3 - use a project-specific output pane

--- src/ResPsuedoLoc/ResPsuedoLoc/OutputPane.cs
+++ src/ResPsuedoLoc/ResPsuedoLoc/OutputPane.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GeneralOutputPane.cs" company="Matt Lacey Ltd.">
+﻿// <copyright file="OutputPane.cs" company="Matt Lacey Ltd.">
 // Copyright (c) Matt Lacey Ltd. All rights reserved.
 // </copyright>
 
@@ -9,48 +9,48 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace ResPsuedoLoc
 {
-    public class GeneralOutputPane
+    public class OutputPane
     {
-        private static GeneralOutputPane instance;
+        private static Guid rplPaneGuid = new Guid("ACC5B167-EB63-465B-8BCE-BFF0CC308009");
 
-        private readonly IVsOutputWindowPane generalPane;
+        private static OutputPane instance;
 
-        private GeneralOutputPane()
+        private readonly IVsOutputWindowPane pane;
+
+        private OutputPane()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var generalPaneGuid = VSConstants.GUID_OutWindowGeneralPane;
-
             if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
-             && (ErrorHandler.Failed(outWindow.GetPane(ref generalPaneGuid, out this.generalPane)) || this.generalPane == null))
+             && (ErrorHandler.Failed(outWindow.GetPane(ref rplPaneGuid, out this.pane)) || this.pane == null))
             {
-                if (ErrorHandler.Failed(outWindow.CreatePane(ref generalPaneGuid, "General", 1, 0)))
+                if (ErrorHandler.Failed(outWindow.CreatePane(ref rplPaneGuid, Vsix.Name, 1, 0)))
                 {
                     System.Diagnostics.Debug.WriteLine("Failed to create the Output window pane.");
                     return;
                 }
 
-                if (ErrorHandler.Failed(outWindow.GetPane(ref generalPaneGuid, out this.generalPane)) || (this.generalPane == null))
+                if (ErrorHandler.Failed(outWindow.GetPane(ref rplPaneGuid, out this.pane)) || (this.pane == null))
                 {
                     System.Diagnostics.Debug.WriteLine("Failed to get access to the Output window pane.");
                 }
             }
         }
 
-        public static GeneralOutputPane Instance => instance ?? (instance = new GeneralOutputPane());
+        public static OutputPane Instance => instance ?? (instance = new OutputPane());
 
         public void Activate()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            this.generalPane?.Activate();
+            this.pane?.Activate();
         }
 
         public void WriteLine(string message)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            this.generalPane?.OutputStringThreadSafe($"{message}{Environment.NewLine}");
+            this.pane?.OutputStringThreadSafe($"{message}{Environment.NewLine}");
         }
     }
 }
