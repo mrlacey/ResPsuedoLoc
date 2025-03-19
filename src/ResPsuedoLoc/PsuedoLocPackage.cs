06ab263e96682307495d90385bdb9f9d2a65aa33
Matt Lacey <matt@mrlacey.co.uk>
2025-03-19 18:14:44 +0000

    add version number to output pane

--- src/ResPsuedoLoc/PsuedoLocPackage.cs
+++ src/ResPsuedoLoc/PsuedoLocPackage.cs
@@ -58,6 +58,9 @@ namespace ResPsuedoLoc
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+			OutputPane.Instance.WriteLine($"{Vsix.Name} v{Vsix.Version}");
+
 			await SurroundCommand.InitializeAsync(this);
 			await ReverseCommand.InitializeAsync(this);
 			await PaddingCommand.InitializeAsync(this);
